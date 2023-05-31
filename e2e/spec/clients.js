36d410d5f04a7e328ad74ed9c9396c5f7d97de06
Martin Reinhardt <contact@martinreinhardt-online.de>
2023-05-31 14:33:01 +0200

    feat(Testing): Adding end2end testing via keycloak local

---
+++ e2e/spec/clients.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import { test } from 'tape';
+import { spawn } from 'node:child_process';
+import path from 'node:path';
+
+test('Should list clients as JSON', (t) => {
+  const cli = spawn(
+    path.join(path.dirname('.'), 'node'),
+    [
+      'dist/index.js',
+      'listClients',
+      'http://localhost:8080',
+      'keycloak-reporter',
+      '3UYhI2hryFwoVtcd7ljlaDuD9HXrGV5r',
+    ],
+    {
+      env: {
+        ...process.env,
+      },
+    }
+  );
+  cli.stdout.on('data', (chunk) => {
+    t.equal(JSON.parse(chunk.toString()).length, 24);
+  });
+  cli.stdout.on('end', () => {
+    t.end();
+  });
+});
