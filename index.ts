93243e750dd238d979bea4437af9a81c24f86679
Martin Reinhardt <contact@martinreinhardt-online.de>
2023-11-01 08:56:19 +0100

    feat(Config): Adding config validation and helm test hook

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { listUsers, listClients } from './src/commands.js';
+export { configTest, listUsers, listClients } from './src/commands.js';
 export { Options } from './lib/client.js';
 export { convertJSON2CSV } from './lib/convert.js';
 export { post2Webhook } from './lib/output.js';
