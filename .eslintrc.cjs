c57d75454119d9bbe4810119f9dc4b4f87c2c716
Martin Reinhardt <contact@martinreinhardt-online.de>
2025-09-26 07:11:56 +0200

    chore: Migrate eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,51 @@
-/* eslint-env node */
-module.exports = {
-  env: {
-    node: true,
-    commonjs: true,
+const { defineConfig } = require('eslint/config');
+
+const globals = require('globals');
+const tsParser = require('@typescript-eslint/parser');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const js = require('@eslint/js');
+
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.commonjs,
+      },
+
+      parser: tsParser,
+    },
+
+    extends: compat.extends(
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ),
+
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    rules: {
+      quotes: [
+        2,
+        'single',
+        {
+          avoidEscape: true,
+        },
+      ],
+
+      'comma-dangle': ['error', 'only-multiline'],
+    },
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-  rules: {
-    quotes: [2, 'single', { avoidEscape: true }],
-    'comma-dangle': ['error', 'only-multiline'],
+  {
+    ignores: ['.eslintrc.cjs', 'dist/*'],
   },
-};
+]);
